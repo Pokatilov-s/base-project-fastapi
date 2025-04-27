fdc0841eb13c8a9b1d51c8cc739266bd4dd0658a
Pokatilov-s <semenpokatilov465@gmail.com>
2025-04-27 22:51:04 +0500

    updated

--- src/core/config.py
+++ src/core/config.py
@@ -1,13 +1,17 @@
 import os
 from pathlib import Path
+from dataclasses import dataclass
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-
+@dataclass
 class Settings:
     PROJECT_NAME: str = "base-project-fastapi"
     PROJECT_VERSION: str = "0.0.0"
 
     BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+settings = Settings()
--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
+import uvicorn
 from fastapi import FastAPI
-from fastapi.responses import ORJSONResponse
-from core import settings
+# from fastapi.responses import ORJSONResponse
+# from core.config import settings
 
 app = FastAPI(
-    title=settings.PROJECT_NAME,
-    version=settings.PROJECT_VERSION,
-    default_response_class=ORJSONResponse
+    # title=settings.PROJECT_NAME,
+    # version=settings.PROJECT_VERSION,
+    # default_response_class=ORJSONResponse
 )
 
 @app.get("/")
 async def hello_world():
     return "hello world"
 
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
